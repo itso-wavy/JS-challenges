fc349ba559f71c243a67d46d54012c1284162c7f
itso-wavy <you.meet.vvavy@gmail.com>
2023-04-04 00:17:25 +0900

    IntegerInput 구현

---
+++ IntegerInput/app.js
@@ -0,0 +1,31 @@
+const input = document.querySelector('#input');
+const display = document.querySelector('#display');
+const text = document.querySelector('#text');
+
+const allowedKeys = {
+  backspace: 8,
+  leftarrow: 37,
+  rightarrow: 39,
+  delete: 46,
+};
+
+const checkInt = e => {
+  display.style.display = 'none';
+  for (let key in allowedKeys) {
+    if (e.keyCode == allowedKeys[key]) {
+      display.style.display = 'block';
+      text.innerText = e.key;
+    } else if (
+      !(48 <= e.keyCode && e.keyCode <= 57) &&
+      !(97 <= e.keyCode && e.keyCode <= 105)
+    )
+      e.preventDefault();
+  }
+};
+
+const checkChar = e => {
+  e.target.value = e.target.value.replace(/[^\d]/gi, '');
+};
+
+input.addEventListener('keydown', checkInt);
+input.addEventListener('keyup', checkChar);
